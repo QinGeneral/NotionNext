c7842af0ea984d8296c7ebe63beb866bf8ce3519
HanZhang <hanzhang@192.168.1.18>
2023-05-28 16:58:47 +0800

    change blog name

--- blog.config.js
+++ blog.config.js
@@ -13,15 +13,15 @@ const BLOG = {
 
     CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'HanZhang', // 您的昵称 例如 tangly1024
-    BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-    LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Element', // 您的昵称 例如 tangly1024
+    BIO: process.env.NEXT_PUBLIC_BIO || '聊聊技术和读书', // 作者简介
+    LINK: process.env.NEXT_PUBLIC_LINK || 'https://qingeneral.github.io/', // 网站地址
+    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 技术, 读书, AI', // 网站关键词 英文逗号隔开
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
     CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
     CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
     CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
-    CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
+    CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/QinGeneral',
     CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
     CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
     CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
@@ -129,7 +129,7 @@ const BLOG = {
     ],
 
     // 樱花飘落特效
-    SAKURA: process.env.NEXT_PUBLIC_SAKURA || false, // 开关
+    SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
 
     // 漂浮线段特效
     NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
@@ -140,7 +140,7 @@ const BLOG = {
     RIBBON: process.env.NEXT_PUBLIC_RIBBON || false, // 开关
 
     // 星空雨特效 黑夜模式才会生效
-    STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || false, // 开关
+    STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || true, // 开关
 
     // 悬浮挂件
     WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
@@ -150,7 +150,7 @@ const BLOG = {
     WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
 
     // 音乐播放插件
-    MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+    MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
     MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
     MUSIC_PLAYER_AUTO_PLAY:
         process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
