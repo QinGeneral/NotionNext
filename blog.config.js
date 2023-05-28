c3307c1d31a711fd7a6474a14ee5d00b46e7c780
HanZhang <hanzhang@192.168.1.18>
2023-05-28 18:00:25 +0800

    change blog name

--- blog.config.js
+++ blog.config.js
@@ -13,16 +13,16 @@ const BLOG = {
 
     CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Element', // 您的昵称 例如 tangly1024
+    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Element', // 您的昵称
     BIO: process.env.NEXT_PUBLIC_BIO || '聊聊技术和读书', // 作者简介
     LINK: process.env.NEXT_PUBLIC_LINK || 'https://qingeneral.github.io/', // 网站地址
     KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, 技术, 读书, AI', // 网站关键词 英文逗号隔开
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-    CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+    CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址
     CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
     CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
     CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/QinGeneral',
-    CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
+    CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址
     CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
     CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
 
@@ -313,7 +313,7 @@ const BLOG = {
 
     // 作废配置
     AVATAR: process.env.NEXT_PUBLIC_AVATAR || '/avatar.svg', // 作者头像，被notion中的ICON覆盖。若无ICON则取public目录下的avatar.png
-    TITLE: process.env.NEXT_PUBLIC_TITLE || 'NotionNext BLOG', // 站点标题 ，被notion中的页面标题覆盖
+    TITLE: process.env.NEXT_PUBLIC_TITLE || 'Element', // 站点标题 ，被notion中的页面标题覆盖
     HOME_BANNER_IMAGE:
         process.env.NEXT_PUBLIC_HOME_BANNER_IMAGE || './bg_image.jpg', // 首页背景大图, 会被notion中的封面图覆盖，若无封面图则会使用代码中的 /public/bg_image.jpg 文件
     DESCRIPTION:
